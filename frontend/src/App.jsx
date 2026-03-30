905bb73fa82fe1cdef380cd9b9e67ee7fb9bea2d
Ole-code-man <ole.magnus.letrud@gmail.com>
2026-03-30 17:06:25 +0200

    added a page wrapper that sepparetes the sides of the website to the main part

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -76,52 +76,54 @@ function App() {
 //Thank you too my girlfreind for drwaing the historic figures<3
    return (
     <>
-    <div className="app-container">
-      <h2>Historic chatbot</h2>
-      <h3>Choose the historic figure you want to talk to</h3>
-      <div className="button-row">
-        <div className="img_buttons">
-          <img src="/images/napoleon_art.png" alt="napoleon.png" />
-          <button onClick={() => changeFigure("Napoleon")}>Napoleon</button>
-        </div>
-        <div className="img_buttons"> 
-          <img src="/images/cleopatra_art.png" alt="cleopatra.png" />
-          <button onClick={() => changeFigure("Cleopatra")}>Cleopatra</button>
-        </div>
-        <div className="img_buttons">
-          <img src="/images/ceasar_art.png" alt="ceasar.png"/>
-          <button onClick={() => changeFigure("Caesar")}>Caesar</button>
+    <div className="page_wrapper">
+      <div className="app-container">
+        <h2>Historic chatbot</h2>
+        <h3>Choose the historic figure you want to talk to</h3>
+        <div className="button-row">
+          <div className="img_buttons">
+            <img src="/images/napoleon_art.png" alt="napoleon.png" />
+            <button onClick={() => changeFigure("Napoleon")}>Napoleon</button>
+          </div>
+          <div className="img_buttons"> 
+            <img src="/images/cleopatra_art.png" alt="cleopatra.png" />
+            <button onClick={() => changeFigure("Cleopatra")}>Cleopatra</button>
+          </div>
+          <div className="img_buttons">
+            <img src="/images/ceasar_art.png" alt="ceasar.png"/>
+            <button onClick={() => changeFigure("Caesar")}>Caesar</button>
+          </div>
         </div>
       </div>
-    </div>
-    <div className="app-container2">
-       <div className="response">
-        {chat.map((msg, index) => (
-          <div key={index} className={`message ${msg.role ==="user"? "message-user" : "message-bot"}`}>
-            <strong>
-              {msg.role === "user" ? "You" : figure}:
-            </strong>{" "}
-            {msg.text}
+      <div className="app-container2">
+        <div className="response">
+          {chat.map((msg, index) => (
+            <div key={index} className={`message ${msg.role ==="user"? "message-user" : "message-bot"}`}>
+              <strong>
+                {msg.role === "user" ? "You" : figure}:
+              </strong>{" "}
+              {msg.text}
+            </div>
+            ))} 
+            {loading && (
+          <p>
+            <strong>{figure}:</strong> <em>thinking...</em>
+          </p>
+  )}
+            <div ref={bottomRef} />
           </div>
-          ))} 
-          {loading && (
-        <p>
-          <strong>{figure}:</strong> <em>thinking...</em>
-        </p>
-)}
-          <div ref={bottomRef} />
+        <div className="input-area">
+          <input
+            type="text"
+            placeholder={`Ask ${figure || "someone"} something`}
+            value={message}
+            onChange={(e) => setMessage(e.target.value)}
+            onKeyDown={handleKeypress}
+            />
+          <button onClick={testAPI} disabled={loading} className="send_button">
+            {loading ? "..." : "SEND"}
+          </button>
         </div>
-      <div className="input-area">
-        <input
-          type="text"
-          placeholder={`Ask ${figure || "someone"} something`}
-          value={message}
-          onChange={(e) => setMessage(e.target.value)}
-          onKeyDown={handleKeypress}
-          />
-        <button onClick={testAPI} disabled={loading} className="send_button">
-          {loading ? "..." : "SEND"}
-        </button>
       </div>
     </div>
     </>
